fc5668f4071f77e34297bdb98d8036143aa60001
vinayak04ma <vinayakmaheshwari57@gmail.com>
2024-10-23 08:45:07 +0530

    com

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=express();
 const PORT =8000;
 
 
-app.get("/api/users",(req,res)=>{
+app.get("/",(req,res)=>{
     return res.json(users)
 })
 app.listen(PORT,()=>console.log(`Server Started at PORT`)
